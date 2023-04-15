1d1cf254580707202cedf2fafc0a3ae7fd756e23
Lê Chí Luân <chiluan6601@gmail.com>
2023-04-15 23:16:54 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,6 @@
 // Connect with MySQL
 //	$conn = mysqli_connect("localhost", "root", "", "db_ideas");
 $conn = mysqli_connect("bjzzesbzxmdkjy99zjrn-mysql.services.clever-cloud.com", "uc2fzankb4vckadp", "JuLLcnGBQKm1k1MN1gxp", "bjzzesbzxmdkjy99zjrn", "3306");
-
 if (!$conn) {
     die("Could not connect to database");
 }

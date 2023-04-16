fe74cdb8480cb8415b18a10d13a19afcc3bfbc1d
Lê Chí Luân <chiluan6601@gmail.com>
2023-04-16 14:34:02 +0700

    update

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 // Connect with MySQL
-//$conn = mysqli_connect("localhost", "root", "", "db_ideas", "3306");
-$conn = mysqli_connect("bjzzesbzxmdkjy99zjrn-mysql.services.clever-cloud.com", "uc2fzankb4vckadp", "JuLLcnGBQKm1k1MN1gxp", "bjzzesbzxmdkjy99zjrn", "3306");
+$conn = mysqli_connect("localhost", "root", "", "db_ideas", "3306");
+//$conn = mysqli_connect("bjzzesbzxmdkjy99zjrn-mysql.services.clever-cloud.com", "uc2fzankb4vckadp", "JuLLcnGBQKm1k1MN1gxp", "bjzzesbzxmdkjy99zjrn", "3306");
 if (!$conn) {
     echo "<script>alert('Connection failed: " . mysqli_connect_error() . "')</script>";
 }
